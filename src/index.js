c9613575452de971725e60862ea49b1d7d8c21e6
paulstgermain <paulg.stgermain@gmail.com>
2021-02-10 17:51:30 -0500

    attempts stretch goal to implement a useMedia hook

--- src/index.js
+++ src/index.js
@@ -6,12 +6,12 @@ import Charts from "./components/Charts";
 import Navbar from "./components/Navbar";
 
 import { useDarkMode } from './hooks/useDarkMode';
+import usePrefersDarkMode from './hooks/usePrefersDarkMode';
 
 import "./styles.scss";
 
 const App = () => {
   const [coinData, setCoinData] = useState([]);
-  // const [darkMode, setDarkMode] = useState(false);
 
   const [darkMode, setDarkMode] = useDarkMode(false);
 
